46cb5045bf4b543a3c497d407cef4facd652883d
SlaveAngelovski <36504139+SlaveAngelovski@users.noreply.github.com>
2018-12-10 21:58:09 +0100

    changed script loading order

--- scripts/globalFunctions.js
+++ scripts/globalFunctions.js
@@ -31,3 +31,12 @@ function getOffsetTop(className) {
         return id(className).getBoundingClientRect().top + document.documentElement.scrollTop;
     }
 }
+
+function scrollToEl(element) {
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': element.offsetTop
+    });
+  }
+  

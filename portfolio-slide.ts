43d4180b7f90dbba1503d783ee904c4353ddb935
Slave Angelovski <slave.angelovski@gmail.com>
2025-10-11 22:01:57 +0200

    feat: adding the portfolio ts functionality

---
+++ portfolio-slide.ts
@@ -0,0 +1,67 @@
+type PortfolioElements = {
+  images: NodeListOf<HTMLImageElement>;
+  description: NodeListOf<HTMLElement>;
+  arrowLeft: HTMLElement | null;
+  arrowRight: HTMLElement | null;
+};
+
+export function initializePortfolio(): void {
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const parent = document.querySelector('[data-portfolio]');
+
+    if (parent === null) return;
+
+    let currentIndex = 0;
+
+    const elements: PortfolioElements = {
+      images: document.querySelectorAll('[data-portfolio] img'),
+      description: document.querySelectorAll('[data-portfolio-item]'),
+      arrowLeft: parent.querySelector('[data-arrow-left]'),
+      arrowRight: parent.querySelector('[data-arrow-right]'),
+    };
+
+    if (hasNullProperty(elements) === true) return;
+
+    const totalItems = elements.images.length;
+
+    elements.arrowLeft.addEventListener('click', () => {
+      currentIndex == 0 ? currentIndex = 0 : currentIndex--;
+      
+      updateDisplay(currentIndex, elements);
+      updateArrowVisibility(currentIndex, totalItems, elements);
+    });
+
+    elements.arrowRight.addEventListener('click', () => {
+      currentIndex >= totalItems - 1 ? currentIndex = totalItems - 1 : currentIndex++;
+
+      updateDisplay(currentIndex, elements);
+      updateArrowVisibility(currentIndex, totalItems, elements);
+    });
+
+    updateArrowVisibility(currentIndex, totalItems, elements);
+  });
+}
+
+function hasNullProperty(element: PortfolioElements): boolean {
+  return Object.values(element).some((value) => {
+    if (value === null) return true;
+    if (value instanceof NodeList && value.length === 0) return true;
+    return false;
+  });
+}
+
+function updateArrowVisibility(index: number, total: number, elements: PortfolioElements): void {
+  elements.arrowLeft.classList.toggle('hidden', index === 0);
+  elements.arrowRight.classList.toggle('hidden', index === total - 1);
+}
+
+function updateDisplay(index: number, elements: PortfolioElements): void {
+  elements.images.forEach((img, i) => {
+    img.classList.toggle('active', i === index);
+  });
+  
+  elements.description.forEach((desc, i) => {
+    desc.classList.toggle('active', i === index);
+  });
+}
